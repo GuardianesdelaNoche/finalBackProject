1e975f28e2e94806c1dc963d8f165d288b2bf51c
Josep Cristobal <josepc.mercader@gmail.com>
2021-07-04 20:06:36 +0200

    JC-1009 Modify User model

--- models/User.js
+++ models/User.js
@@ -4,8 +4,21 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, unique: true},
-    password: String
+    username: {type: String, unique: true, index: true,required: true},
+    email: {type: String, unique: true, index: true,required: true},
+    address: {type: String, index: true },
+    city: {type: String, index: true},
+    postal_code: {type: String, index: true},
+    country: { type: String, index: true },
+    role: {type:Number, index: true,required: true},
+    password: {type: String},
+    phone: {type: String},
+    nickname: {type: String, unique: true, index: true,required: true},
+    image: {type: String},
+    create_date: { type: Date, index: true },
+    my_events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+    suscribe_events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+    fav_events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
 });
 
 userSchema.statics.hashPassword = function(passwordOriginal){
