f077a853fbb52db3462e105eb52eb4a0e91f959f
Josep Cristobal <josepc.mercader@gmail.com>
2021-07-06 19:46:45 +0200

    JC-BACK-1002- register.js

--- routes/api/v1/register.js
+++ routes/api/v1/register.js
@@ -6,6 +6,7 @@ const jwtAuth = require('../../../lib/jwtAuth');
 const {body, validationResult} = require('express-validator');
 const path = require('path');
 const pv = require('password-validator'); //control password restrictions
+const multer = require('multer');
 
 
 const User = require('../../../models/User');
@@ -15,57 +16,82 @@ const passwordSchema = new pv();
 passwordSchema
     .is().min(8)
     .is().max(20)
-    .has().uppercase(2)
+    .has().uppercase(1)
     .has().lowercase()
     .has().digits(1);
 
+/**
+ * To avoid duplicates, we will rename the image as follows
+ * current date + original image name + original extension
+ */
 
+ const storage = multer.diskStorage({
+    destination : './public/images/photoUser',
+    filename: (req,file,cb) =>{
+        cb(null, Date.now() +'-'+file.originalname);
+    }
+});
 
-//Grabación de nuevos anuncios con validación incluida y subida de imagen
-//outer.post('/', jwtAuth, upload,
-router.post('/', jwtAuth,
+const upload = multer({
+    storage,
+    dest: './public/images/photoUser',
+    limits: {fileSize: 1 * 1000000 * 1000000},
+    fileFilter: (req,file,cb) =>{
+        const ext = path.extname(file.originalname).toLowerCase();
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            return cb(new Error('Only images are allowed: .png|.jpg|.gif|.jpeg'))
+        }
+        cb(null, true)
+    },
+}).single('image')
+
+/**
+ * Recording of new events with validation included and image upload
+ */
+
+router.post('/', jwtAuth, upload,
     [
         body('username').isLength({ min: 6 }).escape().withMessage('El campo username es obligatorio'),
         body('email').isEmail().escape().withMessage('El campo email es obligatorio'),
         body('role').isNumeric().withMessage('El rol debe ser numérico'),
         body('nickname').not().isEmpty().trim().escape().withMessage('El nickname es obligatorio'),
         body('password').custom(password => {   
-            
-            //Validamos si la password que nos pasan están dentro de los permitidos
+/**
+ * Validate password, minimum requirements.
+ * Validate email, username, nickname no accept duplicates  
+ */                     
             if ((typeof password != 'undefined') && !passwordSchema.validate(password)){
-                console.log('Password no validado 1')
                 return false
             }else{
-                console.log('Password validado 2 ')
                 return true
             }
         }),
         body('email').custom(async email=>{
             const resultE = await User.existsEmail(email);
             if (resultE >0){
-                throw new Error(`Email duplicado: ${email}`);
+                throw new Error(`Email already exists: ${email}`);
             } else {
                 return true
             }
-        }).escape().withMessage('El campo email, está duplicado'),
+        }).escape().withMessage('E-mail already exists'),
         
         body('username').custom(async username=>{
             const resultU = await User.existsUserName(username);
             if (resultU >0){
-                throw new Error(`Username duplicado: ${username}`);
+                throw new Error(`Username already exists: ${username}`);
             } else {
                 return true
             }
-        }).escape().withMessage('El campo username, está duplicado'),
+        }).escape().withMessage('Username, already exists'),
         
         body('nickname').custom(async nickname=>{
             const resultN = await User.existsNickName(nickname);
             if (resultN >0){
-                throw new Error(`Nickname duplicado: ${nickname}`);
+                throw new Error(`Nickname already exists: ${nickname}`);
             } else {
                 return true
             }
-        }).escape().withMessage('El campo nickname, está duplicado'),
+        }).escape().withMessage('Nickname, already exists'),
         
     ], 
  async (req, res, next) =>{
@@ -74,23 +100,10 @@ router.post('/', jwtAuth,
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array()});
         }
-        // const namePhoto = req.file ? req.file.filename :''
-        
-        // const anuncioCreado = await Anuncio.newAnuncio(req.body,namePhoto);
-        // //Una vez guardado el anuncio, llamamos al microservicio para procesar el thumbnail de la imágen
-        // const nameImage = namePhoto
-    
-        // requester.send({
-        //     type: 'convertir imagen',
-        //     nameImage : nameImage,
-        // }, resultado =>{
-        //     if (!resultado) {
-        //         console.erro('Error en microservice al crear el thumbnail')};
-        //     //console.log(`Cambiamos el nombre de la imágen a : ${resultado}`)
-        // })
+        const namePhoto = req.file ? req.file.filename :''
+        const newUser = await User.newUser(req.body,namePhoto);
 
-        // res.status(201).json({result: anuncioCreado});
-        res.status(201).json({result:'resultado ok' });
+        res.status(201).json({result:newUser});
     } catch (error) {
         next(error)      
         }
