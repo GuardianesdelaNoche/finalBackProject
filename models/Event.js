2fa3115134779535c31c36d8ecc778911771d35a
Joan M <joanmiquelortega@gmail.com>
2021-07-08 13:45:02 +0200

    BACK-1005 add key lat and long in model event

--- models/Event.js
+++ models/Event.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const eventsSchema = mongoose.Schema({
+const eventSchema = mongoose.Schema({
     title: {type: String, index: true, required: true},
     description: {type:String, index: true},
     price: {type:Number, index: true, required: true},
@@ -11,11 +11,36 @@ const eventsSchema = mongoose.Schema({
     duration: { type: Number, index: true, required: true},
     photo: {type:String, index: true},
     indoor: {type:Boolean, index:true, required: true},
+    long: {type:String, index: true},
+    lat: {type:String, index: true},
     tags: [String],
     _id_assistants:[String]
 });
 
+eventSchema.statics.createRecord = function (nuevo, cb) {
+    new Event(nuevo).save(cb)
+  }
+  
+eventSchema.statics.list = async function (filters, startRow, numRows, sortField, includeTotal, cb) {
+    const query = Event.find(filters)
+    query.sort(sortField)
+    query.skip(startRow)
+    query.limit(numRows)
 
-const Event = mongoose.model('Event', eventsSchema);
+    const result = {}
+  
+    if (includeTotal) {
+      result.total = await Event.count()
+    }
+    result.rows = await query.exec()
+  
+    // poner ruta base a imagenes
+    //result.rows.forEach(r => (r.foto = r.foto ? path.join(IMAGE_URL_BASE_PATH, r.foto) : null))
+  
+    if (cb) return cb(null, result) // si me dan callback devuelvo los resultados por ahí
+    return result // si no, los devuelvo por la promesa del async (async está en la primera linea de esta función)
+}
+  
+const Event = mongoose.model('Event', eventSchema);
 
 module.exports = Event;
