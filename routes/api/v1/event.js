9f04bbaa3fa0a7b209136fed2a52245f7411156f
Joan M <joanmiquelortega@gmail.com>
2021-07-08 14:03:17 +0200

    BACK-1005 remove default limit

--- routes/api/v1/event.js
+++ routes/api/v1/event.js
@@ -7,19 +7,19 @@ const Event = require('../../../models/Event')
 router.get('/', async function (req, res, next) {
   try {
     const skip = parseInt(req.query.start) || 0
-    const limit = parseInt(req.query.limit) || 10
+    const limit = parseInt(req.query.limit)
     const sort = req.query.sort || 'date'
     const includeTotal = true
 
     const filters = {}
-    /*
-    if (req.query.tag) {
-      filters.tags = req.query.tag
+    
+    if (req.query.title) {
+      filters.title = new RegExp('^' + req.query.title, 'i')
     }
-    if (req.query.venta) {
-      filters.venta = req.query.venta
+    if (req.query.indoor) {
+      filters.indoor = req.query.indoor
     }
-    */
+    
     const {total, rows} = await Event.list(filters, skip, limit, sort, includeTotal)
     res.json({ total, events: rows })
   } catch (err) { return res.next(err) }
