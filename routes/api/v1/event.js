53be9db4fe6a4bca2421f7880e6a3dd53d92f1cd
Joan M <joanmiquelortega@gmail.com>
2021-07-11 17:39:34 +0200

    BACK-1015 finish endpoint create event

--- routes/api/v1/event.js
+++ routes/api/v1/event.js
@@ -1,7 +1,29 @@
 'use strict'
 
-const router = require('express').Router()
-const Event = require('../../../models/Event')
+const router = require('express').Router();
+const Event = require('../../../models/Event');
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination : './public/images/photoEvent',
+  filename: (req,file,cb) =>{
+      cb(null, Date.now() +'-'+file.originalname);
+  }
+});
+
+const upload = multer({
+  storage,
+  dest: './public/images/photoEvent',
+  limits: {fileSize: 1 * 10000 * 10000},
+  fileFilter: (req,file,cb) =>{
+      const ext = path.extname(file.originalname).toLowerCase();
+      if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+          return cb(new Error('Only images are allowed: .png|.jpg|.gif|.jpeg'))
+      }
+      cb(null, true)
+  },
+}).single('photo')
 
 /* GET events . */
 router.get('/', async function (req, res, next) {
@@ -37,5 +59,32 @@ router.get('/:_id', async function (req, res, next) {
     return res.next(err) 
   }
 })
+
+router.post('/', upload, async (req, res, next) => {
+
+  try {
+    const {title, description, price, max_places, date, duration, indoor, tags, _id_assistants} = req.body;
+
+    const latitude = req.body.latitude ? req.body.latitude : 0
+    const longitude = req.body.longitude ? req.body.longitude : 0
+    const coordinates = latitude<0 && longitude<0 ? [longitude,latitude] :[]
+    const namePhoto = req.file ? req.file.filename :'';
+  
+    
+    const event = new Event({title, description, price, max_places, date, duration, indoor, tags, _id_assistants, 
+                            photo: namePhoto, location: {
+                              type: 'Point',
+                              coordinates: coordinates
+                            }})
+  
+    await event.save();
+  
+    res.status(201).json({ result: event});    
+  } catch (error) {
+    return next(error);
+  }
+
+
+});
 
 module.exports = router
