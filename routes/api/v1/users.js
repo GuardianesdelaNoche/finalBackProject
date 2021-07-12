dfce7818eac5f5c87e4f8570b7567ffc09064b66
Josep Cristobal <josepc.mercader@gmail.com>
2021-07-12 20:12:22 +0200

    JC-BACK-route users add route get user for update

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -11,40 +11,30 @@ const multer = require('multer');
 
 const User = require('../../../models/User');
 
-//Get any user by super administrator
-router.get('/:id_user', jwtAuth, async function(req,res,next){
+//Get any user by super administrator or own 
+router.get('/:id_user?', jwtAuth, async function(req,res,next){
     try {
-        if (req.apiAuthUserRole===9){
-            const resultUserId = await User.getUser(req.params.id_user)
-            const {_id,username} = resultUserId;
-            return res.status(200).json({result: {_id,username}});
-        } else {
-             const err = new Error(`The user does not have privileges for this action`);
+        const idUser = req.params.id_user ? req.params.id_user:req.apiAuthUserId
+        
+        if ((req.apiAuthUserRole===9 && req.params.id_user) || !req.params.id_user){
+            const resultUserId = await User.getUser(idUser)
+            const {_id,username,email,address,city,postal_code,country,role,phone,nickname,image,location} = resultUserId;
+            return res.status(200).json({result: {_id,username,email,address,city,postal_code,country,role,phone,nickname,image,location}});
+        }  else{
+            const err = new Error(`The user does not have privileges for this action`);
             err.status = 403
             throw err
-        }    
+        }  
     } catch (error) {
         next(error);
     }
 })
-
-//Get user to edit
-router.get('/', jwtAuth, async function(req,res,next){
-    try {
-        const resultUser = await User.getUser(req.apiAuthUserId)
-        const {_id,username} = resultUser;
-        return res.status(200).json({result: {_id,username}});
-    } catch (error) {
-        next(error);
-    }
-});
 
 
 //Delete user
 router.delete('/', jwtAuth, async function(req,res,next){
     try {
         const deleteUser = await User.deleteUser(req.apiAuthUserId)
-        const {_id,username} = deleteUser;
         return res.status(200).json({result: `Successful deletion: ${req.apiAuthUserId}`});
     } catch (error) {
         next(error);
