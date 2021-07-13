b8670ff4e33831d343950fb8534f393af4e33a3b
Josep Cristobal <josepc.mercader@gmail.com>
2021-07-13 19:41:50 +0200

    JC-BACK-Add new reoute /api/v1/users

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/api/v1/user/login', authController.postJWT);
 app.use('/api/v1/user/register', require('./routes/api/v1/register'));
 app.use('/api/v1/events', require('./routes/api/v1/event'));
 
-app.use('/api/v1/user/update', require('./routes/api/v1/users'));
+app.use('/api/v1/users', require('./routes/api/v1/users'));
 app.use('/api/v0/routesTest', require('./routes/api/v0/routesTest'));
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
