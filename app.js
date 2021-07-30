e4e023cf9cac4eea9c08937bfc0542335d7246e5
Joan M <joanmiquelortega@gmail.com>
2021-07-30 13:12:39 +0200

    BACK-BACK-3002 add router eventuser in app.js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.post('/api/v1/user/login', authController.postJWT);
 app.use('/api/v1/user/register', require('./routes/api/v1/register'));
 app.use('/api/v1/events', require('./routes/api/v1/event'));
 app.use('/api/v1/tags', require('./routes/api/v1/tag'));
+app.use('/api/v1/eventsuser', require('./routes/api/v1/eventuser'));
 
 
 app.use('/api/v1/users', require('./routes/api/v1/users'));
