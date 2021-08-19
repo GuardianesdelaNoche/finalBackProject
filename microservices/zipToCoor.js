173640a65c76e39c45d252673b4e38a9c9c2fbfa
Josep Cristobal <josepc.mercader@gmail.com>
2021-08-19 11:30:53 +0200

    JC-BACK- Microservice pepe-distcalc

---
+++ microservices/zipToCoor.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const cote = require('cote');
+const distCalc = require('pepe-distcalc')
+
+//Postal code to coordinates conversion service
+
+// Declare microservice
+
+const responder = new cote.Responder({name: 'Transform zipCode'})
+
+const coord = async (country,zipCode) =>{
+    try {
+        const zipPos = await distCalc.codPostToLocation(country,zipCode);
+        console.log('The result is: ',zipPos);
+        return zipPos
+    } catch (error) {
+        console.error(error);
+    }    
+};
+
+//Example country and zip code
+//test4('Spain','08210');
+
+
+//Lógica del microservicio
+responder.on('Transform zipCode', async (req, done) => {
+
+    const zipCode = req.zipCode
+    const country = req.country
+    const resultCoor = await coord(country,zipCode)
+    console.log('El resultado es:', resultCoor)
+    //Devolvemos el nombre de la nueva imágen creada
+    done(resultCoor); 
+});
