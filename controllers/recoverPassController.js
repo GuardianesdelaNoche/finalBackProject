bae3de1a2912a785254c042fd2fadddc34a76adf
Josep Cristobal <josepc.mercader@gmail.com>
2021-08-23 23:10:33 +0200

    JC-BACK- Recover token expiration value in env

--- controllers/recoverPassController.js
+++ controllers/recoverPassController.js
@@ -18,7 +18,7 @@ async function recoverPassController(email){
            
             //if user exists
             //Create a signed token and send to e-mail
-            return jwt.sign({ _id: user._id, role: 99 }, process.env.JWT_SECRET, { expiresIn: 240 });
+            return jwt.sign({ _id: user._id, role: 99 }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_RECOVER});
         } catch(err){
             return err;
         }
